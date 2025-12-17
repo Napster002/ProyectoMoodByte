b4c6d8a558619c8613b024617fa1e17e06cfcf35
Saul GG <152623929+SaulGarciaaaa@users.noreply.github.com>
2025-12-17 20:10:35 +0100

    M- Comienzo de creación de la creación de usuario en aplicacion escritorio
    Sin implementar a la principal

---
+++ MoodByte_Interfaz/MoodByte_Interfaz/MoodByte/CrearUsuario.cs
@@ -0,0 +1,48 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MoodByte
+{
+    public partial class CrearUsuario : Form
+    {
+        public CrearUsuario()
+        {
+            InitializeComponent();
+            cbTipoUsuario.DataSource = Enum.GetValues(typeof(enumTipoUsuario));
+            cbGenero.DataSource=Enum.GetValues(typeof (enumGenero));
+        }
+        private void buttonGuardar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CrearUsuario_Load(object sender, EventArgs e)
+        {
+          tbNombre.Focus();
+          cbGenero.SelectedIndex = 0;
+          cbTipoUsuario.SelectedIndex = 0;
+        }
+
+        private void buttonLimpiar_Click(object sender, EventArgs e)
+        {
+            tbNombre.Clear();
+            tbApellidos.Clear();
+            dtpFechanacimiento.Value = DateTime.Today;
+            cbGenero.SelectedIndex = 0;
+            cbTipoUsuario.SelectedIndex = 0;
+            tbNombreUsuario.Clear();
+            tbContraseña.Clear();
+            tbRepitecontraseña.Clear();
+            tbNombre.Focus();
+        }
+    }
+}

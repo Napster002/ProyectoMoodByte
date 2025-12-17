49f1186410f95ed2dd61f09cbf18c21e92efdf37
Saul GG <152623929+SaulGarciaaaa@users.noreply.github.com>
2025-12-17 17:24:30 +0100

    M- cambio persistence

--- MoodByte/src/main/java/Main/Main.java
+++ MoodByte/src/main/java/Main/Main.java
@@ -1,31 +1,15 @@
 package Main;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import Logica.RegistroDAO;
-=======
-import Conexion.ConexionMysqlEMF;
->>>>>>> Stashed changes
-=======
-import Conexion.ConexionMysqlEMF;
->>>>>>> Stashed changes
 import Logica.UsuarioDAO;
 import Modelo.Genero;
-import Modelo.Registro;
 import Modelo.TipoUsuario;
 import Modelo.Usuario;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       ConexionMysqlEMF.getEmf();
-
-       UsuarioDAO dao=new UsuarioDAO();
+        UsuarioDAO dao=new UsuarioDAO();
         Usuario usuario=new Usuario();
         usuario.setEdad(31);
         usuario.setGenero(Genero.Hombre);
@@ -34,22 +18,5 @@ public class Main {
         usuario.setPassword("Chimichangaaaaaas");
         usuario.setTipoUsuario(TipoUsuario.Administrador);
         dao.insertar(usuario);
-
-        RegistroDAO reo=new RegistroDAO();
-        Registro R1=new Registro();
-        R1.setFechaRegistro(LocalDate.now());
-        R1.setPuntuacion(5);
-        R1.setUsuario(usuario);
-
-        reo.insertar(R1);
-
-        List<Registro> regis = new ArrayList<>();
-        regis.add(R1);
-        usuario.setRegistros(regis);
-        dao.actualizar(usuario);
-
-        List<Registro> lista=dao.mostrarRegistros(usuario);
-        System.out.println("Lista registros del usuario 1:");
-        lista.forEach(r-> System.out.println(r.toString()));
     }
 }

775099a99f2d686e2f6290ab554207d78e0354bd
Saul GG <152623929+SaulGarciaaaa@users.noreply.github.com>
2025-12-17 21:39:01 +0100

    Add files via upload

---
+++ MoodByte_Interfaz/Modelo/Articulo.cs
@@ -0,0 +1,11 @@
+﻿namespace Modelo
+{
+    public class Articulo
+    {
+        public long id { get; set; }
+        public string titulo { get; set; }
+        public string subtitulo { get; set; }
+        public string enlace { get; set; }
+        public string imagen { get; set; }
+    }
+}
---
+++ MoodByte_Interfaz/Modelo/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Usuario
+    {
+        public long Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string NombreCompleto { get; set; }
+        public int Edad { get; set; }
+        public int Nivel { get; set; }
+        public double ExpAcumulada { get; set; }
+        public string Password { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public enumGenero Genero { get; set; }
+        public enumTipoUsuario TipoUsuario { get; set; }
+    }
+}

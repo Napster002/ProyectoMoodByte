4bdbd35211a9a9c6c9f0083ae381d4b50cfd677a
Rubén Cobo González <rubencobog@gmail.com>
2025-12-17 08:33:24 -0800

    1

---
+++ MoodByte_Interfaz/Modelo/Frase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Frase
+    {
+        public long id { get; set; }
+        public string frase { get; set; }
+        public int puntuacion { get; set; }
+    }
+}
---
+++ MoodByte_Interfaz/Modelo/enumTipoUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public enum enumTipoUsuario
+    {
+        Administrador,
+        Cliente_premium,
+        Cliente,
+        Invitado
+    }
+}

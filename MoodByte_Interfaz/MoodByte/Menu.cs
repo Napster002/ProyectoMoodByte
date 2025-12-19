103d952316ee906f50e7f7e8a2918f6edf2d20a7
Napster002 <saizlanderas02@gmail.com>
2025-12-19 17:33:41 +0100

    MenuFuncional

---
+++ MoodByte_Interfaz/MoodByte/Menu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoodByte
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            navMenu.AbrirUsuarios += menu_abrirUsuarios;
+            navMenu.AbrirArticulos += menu_abrirArticulos;
+            navMenu.AbrirEjercicios += menu_abrirEjercicios;
+            navMenu.AbrirFrases += menu_abrirFrases;
+        }
+        private void menu_abrirUsuarios(object sender, EventArgs e) { 
+        
+            AdminUsuarios usuarios = new AdminUsuarios();
+            usuarios.Visible = true;
+            this.Close();
+
+        }
+        private void menu_abrirArticulos(object sender, EventArgs e) { 
+        
+            AdminArticulos articulos = new AdminArticulos();
+            articulos.Visible = true;
+            this.Close();
+           
+        }
+        private void menu_abrirEjercicios(object sender, EventArgs e) { 
+        
+            AdminEjercicios ejercicios = new AdminEjercicios();
+            ejercicios.Visible = true;
+            this.Close();
+
+        }
+        private void menu_abrirFrases(object sender, EventArgs e) { 
+        
+            AdminFrases frases = new AdminFrases();
+            frases.Visible = true;
+            this.Close();
+        
+        }
+    }
+}

78e6e2ad6d39d9e80dd1d85f6c6da3522560fe53
Saul GG <152623929+SaulGarciaaaa@users.noreply.github.com>
2025-12-23 15:55:44 +0100

    M-> Cambios en insertar y login

--- MoodByte_Interfaz/Conexiones/ConexionGenerica.cs
+++ MoodByte_Interfaz/Conexiones/ConexionGenerica.cs
@@ -3,5 +3,6 @@
     // Variable generica por si usas maquinas virtual no cambiarlo en todos lados solo aqui
     public class ConexionGenerica
     {
+        public static string Conexion { get; } = "http://192.168.6.1:5500";
     }
 }
--- MoodByte_Interfaz/MoodByte/Program.cs
+++ MoodByte_Interfaz/MoodByte/Program.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace MoodByte
 {
     internal static class Program
@@ -11,7 +13,8 @@ namespace MoodByte
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AdminArticulos());
+            Application.Run(log);
         }
+        public static Login log = new Login();
     }
 }
